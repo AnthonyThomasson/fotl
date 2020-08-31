0001ff0b81f81ef588d768889ab248479adc4d0e
Anthony Thomasson <athomasson.93@gmail.com>
2020-08-30 23:02:11 -0600

    - first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+app.use(
+  "/public/TemplateData",
+  express.static(__dirname + "/public/TemplateData")
+);
+
+app.use("/public/Build", express.static(__dirname + "/public/Build"));
+
+app.use(express.static(__dirname + "/public"));
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("PING", () => {
+    console.log("Ping received from client");
+    socket.emit("PONG");
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
